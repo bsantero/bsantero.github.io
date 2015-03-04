07bfe41d3b7447c43ed692b489e302051acc5eba
bsantero <brian.santero@gmail.com>
2015-03-04 14:41:53 -0600

    Added behavior to about page for two bios

--- js/title.js
+++ js/title.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
 	console.log("JS loaded");
 
+$(".bio").addClass("hidden");
+
+
+/* About Page */
+
+$(".bio-toggle").on("click", function(){
+	console.log("load "+$(this).data("dest"));
+	$("#"+$(this).data("dest")).siblings().addClass("hidden");
+	$("#"+$(this).data("dest")).removeClass("hidden");
+});
+
+
+
 });
