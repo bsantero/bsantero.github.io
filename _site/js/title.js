0c5d9642a55492d54a6afae1bff838b123fe774b
bsantero <brian.santero@gmail.com>
2015-03-04 00:24:07 -0600

    animation in pure css

--- _site/js/title.js
+++ _site/js/title.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
 	console.log("JS loaded");
 
-	$(".faa-parent").hover(
-		function() {
-			$(this).find("i.fa").addClass("bs-shake");
-		},
-		function() {
-			$(this).find("i.fa").removeClass("bs-shake");
-		});
 });
