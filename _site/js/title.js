e76b32662740bc5d8ab20b45126d9df938b94b65
bsantero <brian.santero@gmail.com>
2015-03-04 16:03:27 -0600

    Bio Animation

--- _site/js/title.js
+++ _site/js/title.js
@@ -1,16 +1,46 @@
 $(document).ready(function(){
 	console.log("JS loaded");
 
-$(".bio").addClass("hidden");
+$("#tech-bio").addClass("hidden");
 
 
 /* About Page */
 
-$(".bio-toggle").on("click", function(){
-	console.log("load "+$(this).data("dest"));
-	$("#"+$(this).data("dest")).siblings().addClass("hidden");
-	$("#"+$(this).data("dest")).removeClass("hidden");
+$("#music-toggle").on("click", function(){
+	if ($("#music-bio").hasClass("hidden")) {
+		$("#tech-bio").addClass("animated fadeOutRightBig").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+			$("#tech-bio").addClass("hidden").removeClass("animated fadeOutRightBig");
+			$("#music-bio").removeClass("hidden").addClass("animated fadeInUpBig").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+			$("#music-bio").removeClass("animated fadeInUpBig");
+			});
+		});
+	}
 });
+
+$("#tech-toggle").on("click", function(){
+	if ($("#tech-bio").hasClass("hidden")) {
+		$("#music-bio").addClass("animated fadeOutLeftBig").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+			$("#music-bio").addClass("hidden").removeClass("animated fadeOutLeftBig");
+			$("#tech-bio").removeClass("hidden").addClass("animated fadeInUpBig").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+			$("#tech-bio").removeClass("animated fadeInUpBig");
+			});
+		});
+	}
+});
+
+// $(".bio-toggle").on("click", function(){
+// 	console.log("hide "+$(this).data('hide'));
+// 	$("#"+$(this).data('hide')).addClass("animated fadeOutLeftBig").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+// 		$("#"+$(this).data('hide')).addClass("hidden");
+// 		$("#"+$(this).data("dest")).removeClass("hidden");
+// 		$("#"+$(this).data("dest")).addClass("animated fadeInRightBig");
+// 	});
+
+// 	// $("#"+$(this).data("dest")).removeClass("hidden");
+// 	// $("#"+$(this).data("dest")).addClass("animated fadeInRightBig").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
+// 		// $(this).removeClass("animated fadeInRightBig");
+// 	// });
+// });
 
 
 
